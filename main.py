a37ec8ff0c3bdc17d24c46142154606752f779f9
edu292 <eduskoroboatei@gmail.com>
2025-04-28 19:35:45 -0300

    Latest Implementation

---
+++ main.py
@@ -0,0 +1,157 @@
+import pygame
+from sudoku_board_reader import SudokuBoardReader
+
+SCREEN_SIZE = 750
+CELL_GAP = 2
+BLOCK_GAP = 3
+OUTLINE_WIDTH = CELL_GAP+BLOCK_GAP
+
+
+class Game:
+    def __init__(self, screen_size):
+        self.board = Board()
+        self.reader = SudokuBoardReader()
+        self.cell_size = (screen_size - (self.board.size + 2) * CELL_GAP) // self.board.size
+        self.font = pygame.font.Font(size=50)
+        self.selected_cell = False
+        self.selected_cell_index = None
+        self.outline_rect = None
+
+    def clear_outline(self):
+        pygame.draw.rect(window, (0, 0, 0), self.outline_rect, width=OUTLINE_WIDTH)
+
+    def mouse_click(self, pos):
+        row = pos[1]//self.cell_size
+        column = pos[0]//self.cell_size
+        if self.selected_cell:
+            self.clear_outline()
+        self.selected_cell = True
+        self.selected_cell_index = (row, column)
+
+    def place_number(self, number):
+        if not self.selected_cell:
+            return
+        if self.board.place(number, self.selected_cell_index[0], self.selected_cell_index[1]):
+            self.clear_outline()
+            self.selected_cell = False
+
+    def erase(self):
+        self.board.erase(self.selected_cell_index[0], self.selected_cell_index[1])
+
+    def get_board_from_image(self):
+        self.reader.load_image_file()
+        if self.reader.loaded:
+            self.board.load(self.reader.board)
+
+    def solve(self):
+        self.board.solve()
+
+    def draw(self):
+        y = 0
+        for row in range(self.board.size):
+            x = 0
+            if row % 3 == 0:
+                y += BLOCK_GAP
+            for column in range(self.board.size):
+                if column % 3 == 0:
+                    x += BLOCK_GAP
+                number = str(self.board.content[row][column])
+                cell_rect = pygame.Rect(x, y, self.cell_size, self.cell_size)
+                if self.selected_cell:
+                    if row == self.selected_cell_index[0] and column == self.selected_cell_index[1]:
+                        self.outline_rect = cell_rect.inflate(OUTLINE_WIDTH, OUTLINE_WIDTH)
+                pygame.draw.rect(window, (255, 255, 255), cell_rect)
+                if number != '0':
+                    text_surface = self.font.render(number, True, (0, 0, 0))
+                    window.blit(text_surface, text_surface.get_rect(center=cell_rect.center))
+                x += self.cell_size + CELL_GAP
+            y += self.cell_size + CELL_GAP
+        if self.selected_cell:
+            pygame.draw.rect(window, (255, 0, 0), self.outline_rect, width=OUTLINE_WIDTH)
+
+
+class Board:
+    def __init__(self):
+        self.size = 9
+        self.content = [[0 for _ in range(self.size)] for _ in range(self.size)]
+
+    def load(self, board):
+        if len(board) == self.size*self.size:
+            self.content = [board[i:i+self.size] for i in range(0, self.size*self.size, self.size)]
+
+    def place(self, number, row, column):
+        if self.is_valid(number, row, column):
+            self.content[row][column] = number
+            return True
+        return False
+
+    def is_valid(self, number, selected_row, selected_column):
+        for column in range(self.size):
+            if self.content[selected_row][column] == number:
+                return False
+        for row in range(self.size):
+            if self.content[row][selected_column] == number:
+                return False
+        block_row = selected_row // 3
+        block_column = selected_column // 3
+        for i in range(3*block_row, 3*block_row+1):
+            for j in range(3*block_column, 3*block_column+1):
+                if self.content[i][j] == number:
+                    return False
+        return True
+
+    def find_empty(self):
+        for row in range(self.size):
+            for column in range(self.size):
+                if self.content[row][column] == 0:
+                    return row, column
+        return -1, -1
+
+    def erase(self, row, column):
+        self.content[row][column] = 0
+
+    def solve(self):
+        row, column = self.find_empty()
+        if row == -1:
+            return True
+
+        for number in range(1, 10):
+            if self.place(number, row, column):
+                if self.solve():
+                    return True
+
+            self.content[row][column] = 0
+
+        return False
+
+
+pygame.init()
+window = pygame.display.set_mode((SCREEN_SIZE, SCREEN_SIZE))
+
+clock = pygame.Clock()
+game = Game(SCREEN_SIZE)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                game.mouse_click(pygame.mouse.get_pos())
+        elif event.type == pygame.KEYDOWN:
+            if event.type == pygame.K_ESCAPE:
+                running = False
+            elif event.unicode.isdigit() and event != '0':
+                game.place_number(int(event.unicode))
+            elif event.key == pygame.K_TAB:
+                game.solve()
+            elif event.key == pygame.K_q:
+                game.get_board_from_image()
+            elif event.key == pygame.K_BACKSPACE:
+                game.erase()
+    game.draw()
+    pygame.display.update()
+    clock.tick(60)
+
+pygame.quit()
---
+++ sudoku_board_reader.py
@@ -0,0 +1,178 @@
+import numpy as np
+import cv2
+from scipy import ndimage
+import onnxruntime as ort
+from tkinter.filedialog import askopenfilename
+
+
+IMG_WIDTH = 450
+IMG_HEIGHT = 450
+model_path = 'mnist-12.onnx'
+
+
+class SudokuBoardReader:
+    def __init__(self):
+        self.session = ort.InferenceSession(model_path)
+        self.input_name = self.session.get_inputs()[0].name
+        self.output_name = self.session.get_outputs()[0].name
+        self.board = None
+        self.loaded = False
+
+    def reset(self):
+        self.board = [0 for _ in range(81)]
+
+    def load_image_file(self):
+        file_path = askopenfilename(title="Select a sudoku board image",
+                                    filetypes=[("Image files", "*.png;*.jpg;*.jpeg;*.webp")])
+        if not file_path:
+            return
+
+        img = cv2.imread(file_path)
+        self.get_board(img)
+
+    def get_board(self, img):
+        self.reset()
+        self.loaded = False
+        img = cv2.resize(img, (IMG_WIDTH, IMG_HEIGHT))
+
+        processed_image = pre_process_image(img)
+        board_image = extract_board(img, processed_image)
+
+        cells = split_cells(board_image)
+        self.recognize_digits(cells)
+        self.loaded = True
+
+    def recognize_digits(self, cells):
+        for idx, cell in enumerate(cells):
+            digit_tensor = preprocess_cell(cell)
+            if digit_tensor is None:
+                continue
+            predictions = self.session.run([self.output_name], {self.input_name: digit_tensor})
+            predicted_digit = int(np.argmax(predictions[0]))
+            self.board[idx] = predicted_digit
+
+
+def pre_process_image(img):
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_blur = cv2.GaussianBlur(img_gray, (5, 5), 1)
+    img_threshold = cv2.adaptiveThreshold(img_blur, 255,
+                                          cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                          cv2.THRESH_BINARY_INV, 11, 2)
+    return img_threshold
+
+
+def extract_board(img, img_threshold):
+    contours, _ = cv2.findContours(img_threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    biggest_rect = None
+    max_area = 0
+    for cnt in contours:
+        x, y, w, h = cv2.boundingRect(cnt)
+        area = w * h
+        if area > max_area:
+            biggest_rect = (x, y, w, h)
+            max_area = area
+
+    x, y, w, h = biggest_rect
+    cropped_board = img[y:y + h, x:x + w]
+
+    resized_board = cv2.resize(cropped_board, (IMG_WIDTH, IMG_HEIGHT))
+    resized_board = cv2.cvtColor(resized_board, cv2.COLOR_BGR2GRAY)
+    return resized_board
+
+
+def split_cells(grid):
+    rows = np.vsplit(grid, 9)
+    cells = [cell for row in rows for cell in np.hsplit(row, 9)]
+    return cells
+
+
+def preprocess_cell(cell):
+    cell = crop_cell_margins(cell)
+    binary = binarize_image(cell)
+
+    digit = extract_digit(binary)
+    if digit is None:
+        return None
+
+    digit_resized = resize_and_center(digit)
+    centered = center_by_mass(digit_resized)
+    norm = centered.astype(np.float32) / 255.0
+
+    return norm.reshape(1, 1, 28, 28)
+
+
+def is_inside_margin(x, y, w, h, img_shape, margin=1):
+    H, W = img_shape
+    return x <= margin or y <= margin or x + w >= W - margin or y + h >= H - margin
+
+
+def extract_digit(thresh):
+    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    if not contours:
+        return None
+
+    h_img, w_img = thresh.shape
+    valid_contours = []
+
+    for cnt in contours:
+        x, y, w, h = cv2.boundingRect(cnt)
+        area = cv2.contourArea(cnt)
+        if is_inside_margin(x, y, w, h, (h_img, w_img)):
+            continue
+        valid_contours.append((cnt, area))
+
+    if not valid_contours:
+        return None
+
+    best_cnt = max(valid_contours, key=lambda x: x[1])[0]
+    x, y, w, h = cv2.boundingRect(best_cnt)
+    if h < 15:
+        return None
+
+    digit = thresh[y:y + h, x:x + w]
+    digit = cv2.copyMakeBorder(digit, 4, 4, 4, 4, cv2.BORDER_CONSTANT, value=0)
+    return digit
+
+
+def binarize_image(cell):
+    if np.mean(cell) > 127:
+        cell = cv2.bitwise_not(cell)
+    cell = cv2.GaussianBlur(cell, (3, 3), 0)
+    _, thresh = cv2.threshold(cell, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+    return thresh
+
+
+def crop_cell_margins(cell, margin=4):
+    return cell[margin:-margin, margin:-margin]
+
+
+def resize_and_center(digit, size=20):
+    h, w = digit.shape
+    scale = size / max(h, w)
+    img_resized = cv2.resize(digit, (int(w * scale), int(h * scale)), interpolation=cv2.INTER_AREA)
+    h, w = img_resized.shape
+
+    digit_resized = np.zeros((size, size), dtype=np.uint8)
+    x_offset = (size - w) // 2
+    y_offset = (size - h) // 2
+    digit_resized[y_offset:y_offset + h, x_offset:x_offset + w] = img_resized
+    return digit_resized
+
+
+def center_by_mass(digit):
+    canvas = np.zeros((28, 28), dtype=np.uint8)
+    canvas[4:24, 4:24] = digit
+
+    cy, cx = ndimage.center_of_mass(canvas)
+    if np.isnan(cx) or np.isnan(cy):
+        return
+
+    rows, cols = canvas.shape
+    shiftx = int(np.round(cols / 2.0 - cx))
+    shifty = int(np.round(rows / 2.0 - cy))
+
+    M = np.float32([[1, 0, shiftx], [0, 1, shifty]])
+    centered = cv2.warpAffine(canvas, M, (cols, rows), borderValue=0)
+    return centered
